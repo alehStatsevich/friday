aa07109614fb13524d1da27fec35c8422c3afb38
olegStacevich <oleg295832611@gmail.com>
2021-06-23 18:56:36 +0200

    profile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import {NavLink} from "react-router-dom";
 import {PATH} from "./Routes.";
 import s from './Header.module.css'
 
+
 function Header() {
     return (
         <div className={s.header}>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+import SuperInputText from "./Input/Input";
+import s from './Header.module.css'
+import SuperCheckbox from "./checkbox/Checkbox";
+import SuperButton from "./button/Button";
+
+
 
 function Profile() {
+    const [checked, setChecked] = useState<boolean>(false)
     return (
         <div>
-            Profile
+            <SuperInputText
+                className={s.blue}
+            />
+            <div>
+                <SuperCheckbox   checked={checked}
+                                 onChangeChecked={setChecked}/>
+            </div>
+            <div>
+                <SuperButton>
+                    default
+                </SuperButton>
+            </div>
         </div>
+
     );
 }
 
